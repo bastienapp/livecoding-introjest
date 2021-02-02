9104ec9be56897e17c19c01db4fabae3a3b2c4e6
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-02-02 08:38:58 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const cart = {
+  items: [
+    {
+      img: '/img/apple_640x426.jpg',
+      name: 'Apple',
+      price: '0.26',
+      count: 2,
+      tax_rate: 0.2,
+    },
+    {
+      img: '/img/orange_640x426.jpg',
+      name: 'Orange',
+      price: '0.42',
+      count: 1,
+      tax_rate: 0.2,
+    },
+    {
+      img: '/img/pear_640x426.jpg',
+      name: 'Pear',
+      price: '0.33',
+      count: 4,
+      tax_rate: 0.2,
+    },
+  ],
+};
+
+// expected
+console.log('Total TTC: 2,68 €');
